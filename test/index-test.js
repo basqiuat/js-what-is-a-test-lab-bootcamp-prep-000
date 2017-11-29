4012a3a3672fbf4aac8446a4bc51ceaf1bc2326f
TIRAS JORDAN <tiraspjordan@gmail.com>
2017-11-29 17:17:49 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -9,8 +9,8 @@ describe('this-is-a-test-name', () => {
 
 
   describe('Height', () => {
-    it('is less than 40', () => {
-      expect(index.height).toBeLessThan(40)
+    it('is more than 40', () => {
+      expect(index.height).toBemoreThan(40)
     })
   })
 
